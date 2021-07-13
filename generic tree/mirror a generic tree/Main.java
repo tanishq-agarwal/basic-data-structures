962735861054c3735d54504044858ae1665ad729
tanishq-agarwal <agarwaltanishq35@gmail.com>
2021-07-13 15:03:34 +0530

    updated file

--- generic tree/mirror a generic tree/Main.java
+++ generic tree/mirror a generic tree/Main.java
@@ -123,8 +123,11 @@ public class Main {
     }
   }
 
-  public static void mirror(Node node){
-    // write your code here
+  public static void mirror(Node node) {
+    for (Node child : node.children) {
+      mirror(child);
+    }
+    Collections.reverse(node.children);
   }
 
   public static void main(String[] args) throws Exception {
